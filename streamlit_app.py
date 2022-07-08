d33e2c3d04664a82f44a158c16066f09680f1b43
GautamGupta17 <98394414+GautamGupta17@users.noreply.github.com>
2022-07-08 18:12:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,20 @@
 import streamlit as st
 
 #### app
+
+header = st.beta_container()
+dataset = st.beta_container()
+features = st.beta_container()
+model_training = st.beta_container()
+
+with header:
+    st.title('Welcome to my IOT PROJECT')
+    st.text('Here we will see how good is our cooked dal sample')
+with dataset:
+    st.header('Dal Dataset I created')
+
+with features:
+    st.header('The features I created')
+
+with model_training:
+    st.header('Time to train the model')
